7a0082a000dfff7b9052e46902ce14d0f2b8e8b0
Prakash Chandra <prakash140chandra@gmail.com>
2023-05-15 14:08:58 +0530

    Update grantAccessTo.js

--- middlewares/grantAccessTo.js
+++ middlewares/grantAccessTo.js
@@ -1,3 +1,37 @@
+function grantAccessTo(roles) {
+  return function (req, res, next) {
+    //Write your code here;
+    const token = req.headers.authorization;
+    if (!token) {
+      return res
+        .status(401)
+        .json({
+          message: "Authentication failed: Missing token.",
+          status: "Error",
+        });
+    }
+
+    try {
+      const decodedToken = jwt.verify(token, JWT_SECRET);
+      if (!roles.includes(decodedToken.role)) {
+        return res
+          .status(403)
+          .json({ message: "Access Denied", status: "Error" });
+      }
+      next();
+    } catch (err) {
+      return res
+        .status(401)
+        .json({
+          message: "Authentication failed: Invalid token.",
+          status: "Error",
+        });
+    }
+  };
+}
+
+module.exports = grantAccessTo;
+
 // const jwt = require('jsonwebtoken');
 
 // const JWT_SECRET = 'newtonSchool';
@@ -38,36 +72,3 @@ Possible Cases:
 3. Role Doesn't Match: { message: 'Access Denied', status: "Error" }
 */
 
-function grantAccessTo(roles) {
-  return function (req, res, next) {
-    //Write your code here;
-    const token = req.headers.authorization;
-    if (!token) {
-      return res
-        .status(401)
-        .json({
-          message: "Authentication failed: Missing token.",
-          status: "Error",
-        });
-    }
-
-    try {
-      const decodedToken = jwt.verify(token, JWT_SECRET);
-      if (!roles.includes(decodedToken.role)) {
-        return res
-          .status(403)
-          .json({ message: "Access Denied", status: "Error" });
-      }
-      next();
-    } catch (err) {
-      return res
-        .status(401)
-        .json({
-          message: "Authentication failed: Invalid token.",
-          status: "Error",
-        });
-    }
-  };
-}
-
-module.exports = grantAccessTo;
