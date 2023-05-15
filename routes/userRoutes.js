4b3d66687fab446729e872845c9e7414fb2ddda3
Prakash Chandra <prakash140chandra@gmail.com>
2023-05-15 20:30:56 +0530

    Update userRoutes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,3 +1,33 @@
+// const express = require("express");
+
+// const {
+//     getAllUsers,
+//     getUserByID,
+//     createUser,
+//     updateUser,
+//     deleteUser,
+// } = require("../controllers/userControllers");
+
+// const grantAccessTo = require("../middlewares/grantAccessTo");
+
+// const router = express.Router();
+
+// /*
+// router.get("/"): Should be accessible to roles user, admin, and superadmin. This route is used to retrieve all users.
+// router.get("/:id"): Should be accessible to roles user, admin, and superadmin. This route is used to retrieve a user by their ID.
+// router.post("/"): Should be accessible to roles guest, user, admin, and superadmin. This route is used to create a new user.
+// router.patch("/:id"): Should be accessible to roles admin and superadmin. This route is used to update an existing user.
+// router.delete("/:id"): Should be accessible to role superadmin only. This route is used to delete an existing user.
+// */
+
+// //Add middlewares here
+// router.get("/", getAllUsers);
+// router.get("/:id", getUserByID);
+// router.post("/", createUser);
+// router.patch("/:id", updateUser);
+// router.delete("/:id", deleteUser);
+
+// module.exports = router;
 const express = require("express");
 
 const {
@@ -21,10 +51,10 @@ router.delete("/:id"): Should be accessible to role superadmin only. This route 
 */
 
 //Add middlewares here
-router.get("/", getAllUsers);
-router.get("/:id", getUserByID);
-router.post("/", createUser);
-router.patch("/:id", updateUser);
-router.delete("/:id", deleteUser);
+router.get("/",grantAccessTo(["user","admin","superadmin"]), getAllUsers);
+router.get("/:id",grantAccessTo(["user","admin","superadmin"]) ,getUserByID);
+router.post("/",grantAccessTo(["user","guest","admin","superadmin"]), createUser);
+router.patch("/:id",grantAccessTo(["admin","superadmin"]), updateUser);
+router.delete("/:id",grantAccessTo(["superadmin"]), deleteUser);
 
 module.exports = router;
